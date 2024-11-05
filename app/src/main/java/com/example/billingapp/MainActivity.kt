c1c6d2b7efc120a1bda35fa0002c9a175ce54ff5
ひじき <87563197+Hijikinoheya@users.noreply.github.com>
2024-11-05 22:00:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/billingapp/MainActivity.kt
@@ -0,0 +1,23 @@
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.webViewClient = WebViewClient()
+
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+
+        webView.loadUrl("http://192.168.71.129/admin/bill_list.php")
+    }
+}
